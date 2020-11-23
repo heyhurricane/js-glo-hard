610d6724875c6697315777be42f62768ff389509
heyhurricane <dinohka@mail.ru>
2020-11-23 21:38:07 +0400

    use reduce

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
+"use strict";
+
 let num = 266219;
 
-var result = 1;
+let array1 = num.toString().split("");
+console.log('array1: ', array1);
 
-for (let i=0; i<num.toString().length; i++) {
-  result *= num.toString()[i];
+const reducer = (accumulator, currentValue) => accumulator * currentValue;
+let result = array1.reduce(reducer);
+
+// var result = 1;
+
+// for (let i=0; i<num.toString().length; i++) {
+//   result *= num.toString()[i];
   
-}
+// }
 
 console.log('result: ', result);
 
 result = result ** 3;
 
-alert(result.toString().slice(0, 2));
+// alert(result.toString().slice(0, 2));
 console.log(result.toString().slice(0, 2));
