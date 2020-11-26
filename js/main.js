773f77b33fa9400326bbdc7a92a30053bd7839f0
heyhurricane <dinohka@mail.ru>
2020-11-26 21:45:43 +0400

    fix some bugs

--- js/main.js
+++ js/main.js
@@ -4,50 +4,59 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+function game() {
+  let num = 43;
+  let count = 10;
 
-function guessTheNumber(num, count) {
-  if (count === 0) {
-    const userConfirm = confirm('Попытки закончились, хотите сыграть еще?');
-    if (userConfirm === true) {
-      count = 10;
-      num = Math.floor(Math.random() * 100) + 1;
-      guessTheNumber(num, count);
-    }
-  }
-  else {
-    let userNum = prompt('Угадай число от 1 до 100');
-    console.log('userNum: ', userNum);
-    if (userNum === null) {
-      alert('Игра окончена');
-    }
-    else
-    {
-      if (!isNumber(userNum)) {
-        alert('Введи число!');
-        guessTheNumber(num, count);
+  function guessTheNumber() {
+    if (count === 0) {
+      let userConfirm = confirm('Попытки закончились, хотите сыграть еще?');
+      if (userConfirm === true) {
+        // console.log('userConfirm: ', userConfirm);
+        count = 10;
+        num = Math.floor(Math.random() * 100) + 1;
+        guessTheNumber();
       }
-      else {
-        if (userNum > num) {
-          count--;
-          alert('Загаданное число меньше, осталось попыток ' + count);
-          guessTheNumber(num, count);
+    }
+    else {
+      let userNum = prompt('Угадай число от 1 до 100');
+      // console.log('userNum: ', userNum);
+      if (userNum === null) {
+        alert('Игра окончена');
+      }
+      else
+      {
+        if (!isNumber(userNum)) {
+          alert('Введи число!');
+          guessTheNumber();
         }
-        if (userNum < num) {
-          count--;
-          alert('Загаданное число больше, осталось попыток ' + count);
-          guessTheNumber(num, count);
-        }
-        if (+userNum === num) {
-          const userConfirm = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-          if (userConfirm === true) {
-            count = 10;
-            num = Math.floor(Math.random() * 100) + 1;
-            guessTheNumber(num, count);
+        else {
+          if (userNum > num) {
+            count--;
+            alert('Загаданное число меньше, осталось попыток ' + count);
+            guessTheNumber();
+          }
+          if (userNum < num) {
+            count--;
+            alert('Загаданное число больше, осталось попыток ' + count);
+            guessTheNumber();
+          }
+          if (+userNum === num) {
+            let userConfirm = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+            // console.log('userConfirm: ', userConfirm);
+            if (userConfirm === true) {
+              count = 10;
+              num = Math.floor(Math.random() * 100) + 1;
+              guessTheNumber();
+            }
           }
         }
-      }
-    } 
+      } 
+    }
   }
+
+  return guessTheNumber();
 }
 
-guessTheNumber(43, 10);
+
+game();
