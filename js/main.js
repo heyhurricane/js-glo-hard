d7a2f5da04163b91c9b5c1b22a6f728496e4ce0f
heyhurricane <dinohka@mail.ru>
2020-11-26 18:14:30 +0400

    add function

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 'use strict';
 
-const arr = ['23510', '12656', '3456', '42365', '50012', '24635', '78965'];
-
-let arraySearch = function(data) {
-  for (let i = 0; i < data.length; i++) {
-    if (data[i][0] === '2' || data[i][0] === '4') {
-      console.log(data[i]);
-    }
-  }
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-arraySearch(arr);
+// let userNum;
 
-let simpleInt = function() {
-  for (let i = 2; i <= 100; i++) {
-    let count = 0;
-    for (let j = 2; j < i; j++) {
-      if ((i % j) === 0) { 
-        count++;
+function guessTheNumber() {
+  const number = 43;
+  let userNum = prompt('Угадай число от 1 до 100');
+  console.log('userNum: ', userNum);
+  if (userNum === null) {
+    alert('Игра окончена');
+  }
+  else
+  {
+    if (!isNumber(userNum)) {
+      alert('Введи число!');
+      guessTheNumber();
+    }
+    else {
+      if (userNum > number) {
+        alert('Загаданное число меньше');
+        guessTheNumber();
+      }
+      if (userNum < number) {
+        alert('Загаданное число больше');
+        guessTheNumber();
+      }
+      if (+userNum === number) {
+        confirm('Поздравляю, Вы угадали!!!');
       }
     }
-    if (count === 0) {
-      console.log(i + ' Делители этого числа: 1 и ' + i);
-    }
-  }
-};
+  } 
+}
 
-simpleInt();
+let res = guessTheNumber();
