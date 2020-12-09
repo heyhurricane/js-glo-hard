6b227fc2a0295c1c00b238e352ac18595b47909f
heyhurricane <dinohka@mail.ru>
2020-12-09 18:20:08 +0400

    add function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
+let newElem;
+
+function DomElement(selector, height, width, bg, fontSize, options) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = options;
 }
- 
+
 DomElement.prototype.createElem = function() {
   let name = '';
-  let newElem;
+  
   const str = this.selector;
     for (let i = 1; i < str.length; i++) {
       name += str[i];
@@ -24,9 +27,39 @@ DomElement.prototype.createElem = function() {
     newElem.setAttribute('id', name);
   }
   newElem.style.cssText = 'background-color: ' + this.bg + '; width: ' + this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;';
+  newElem.style.position = this.position;
+  newElem.style.top = 0;
+  newElem.style.left = 0;
   newElem.textContent = 'Новый элемент создан';
+  console.log('Элемент добавлен');
   document.body.append(newElem);
 };
 
-let elem = new DomElement('.block', 100, 200, '#eeeeee', 14);
-elem.createElem();
+const elem = new DomElement('.block', 100, 100, '#eeeeee', 14, 'absolute');
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('Страница загружена');
+  });
+
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowUp') {
+      newElem.style.top = parseInt(newElem.style.top) - 10 + 'px';
+    }
+    else if (event.code === 'ArrowDown') {
+      newElem.style.top = parseInt(newElem.style.top) + 10 + 'px';
+    }
+    else if (event.code === 'ArrowLeft') {
+      newElem.style.left = parseInt(newElem.style.left) - 10 + 'px';
+    }
+    else if (event.code === 'ArrowRight') {
+      newElem.style.left = parseInt(newElem.style.left) + 10 + 'px';
+    }
+
+}); 
+
+
+
+setTimeout(function(){
+ elem.createElem();
+});
+ 
