5808792630307867829e030ac64e149befdbf463
heyhurricane <dinohka@mail.ru>
2020-12-15 13:43:50 +0400

    add animate

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 'use strict';
 
-const input = document.querySelector('input'),
-text = document.querySelector('.text');
+const bee = document.querySelector('.bee'),
+btnGo = document.querySelector('.go'),
+btnReset = document.querySelector('.reset');
+let count = 0, count2 = 0,
+flyInterval;
 
-function debounce(f, t) {
-  return function (args) {
-    let previousCall = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall && ((this.lastCall - previousCall) <= t)) {
-      clearTimeout(this.lastCallTimer);
+function beeFlight() {
+  flyInterval = requestAnimationFrame(beeFlight);
+  count++;
+  if (count < 400) {
+    bee.style.left = 2*count + 'px';
+  }
+  else {
+    if (count < 700) {
+      count2++;
+      bee.style.top = count2 + 'px';
     }
-    this.lastCallTimer = setTimeout(() => f(args), t);
-  };
+    else { cancelAnimationFrame(flyInterval);}
+  }
+  
 }
 
+let animate = false;
 
-function textOutput() {
-    text.textContent = input.value;
+btnGo.addEventListener('click', () => {
+  if (!animate) {
+    flyInterval = requestAnimationFrame(beeFlight);
+    animate = true;
+    btnGo.textContent = 'Остановить';
   }
+  else {
+    animate = false;
+    btnGo.textContent = 'Запустить';
+    cancelAnimationFrame(flyInterval);
+  }
+});
 
- // setTimeout(textOutput, 300);
+btnReset.addEventListener('click', () => {
+  count = 0; 
+  count2 = 0;
+  bee.style.left = count + 'px';
+  bee.style.top = count2 + 'px';
+  cancelAnimationFrame(flyInterval);
+  animate = false;
+  btnGo.textContent = 'Запустить';
+});
 
-input.addEventListener('keydown', debounce(textOutput, 300)); 
